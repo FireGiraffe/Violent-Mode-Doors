94316ff8de9d6c4945b74c131b460b7b96e3de30
FireGiraffe <firegiraffegaming@gmail.com>
2026-09-22 20:32:09 -0500

    Update Sprint.lua

--- Sprint.lua
+++ Sprint.lua
@@ -98,9 +98,8 @@ function QCHWWK_fake_script()
 	local bar = script.Parent.Border.Bar
 
 	local stamPower = 100
-	local stamTick = 0.6 -- Lowered: Drains slower so sprinting lasts longer
-	local regenTick = 0.25 -- Lowered: Takes longer to regenerate stamina
-
+	local stamTick = 0.3
+	local regenTick = 0.25
 	local sprinting = false
 	local DEF = 15
 	local Walkspeed2 = DEF
