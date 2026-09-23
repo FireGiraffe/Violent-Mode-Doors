bf159cd7be0f23a4bd521682ce381595a771187d
FireGiraffe <firegiraffegaming@gmail.com>
2026-09-22 20:50:20 -0500

    Update Thrash.lua

--- Thrash.lua
+++ Thrash.lua
@@ -1,67 +1,7 @@
 local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/DOORS-Entity-Spawner-V2/main/init.luau"))()
 local TweenService = game:GetService("TweenService")
 
-local Thrash = Spawner:Create({
-	Entity = {
-		Name = "Thrash",
-		Asset = "https://github.com/FireGiraffe/Violent-Mode-Doors/raw/main/Thrash.rbxm",
-		HeightOffset = 0
-	},
-	Movement = {
-		Speed = 280,
-		Delay = 0.6,
-		Reversed = false
-	},
-	Damage = {
-		Enabled = true,
-		IgnoreHiding = false,
-		Range = 45,
-		Amount = 125
-	},
-	Rebounding = {
-		Enabled = true,
-		Type = "Ambush",
-		Min = 3,
-		Max = 6,
-		Delay = 1.4
-	},
-	Lights = {
-		Flicker = {
-			Enabled = false,
-			Duration = 0
-		},
-		Shatter = false,
-		Repair = false
-	},
-	Earthquake = {
-		Enabled = false
-	},
-	CameraShake = {
-		Enabled = true,
-		Values = {3.2, 28, 0.08, 1.4},
-		Range = 120
-	},
-	Crucifixion = {
-		Type = "Curious",
-		Enabled = true,
-		Range = 46,
-		Resist = false,
-		Break = true
-	},
-	Death = {
-		Type = "Curious",
-		Hints = {
-			"Oh, hello...",
-			"You died to Thrash.",
-			"It moves around violently, coming back a few times.",
-			"His indication is the lights flickering a little more than usual.",
-			"Hope you don't mind trying again, it would be helpful."
-		},
-		Cause = "Thrash"
-	}
-})
-
-Thrash:SetCallback("OnSpawned", function()
+local function turnLightsRed()
 	local lights = {}
 	local neons = {}
 	local rooms = {}
@@ -96,6 +36,70 @@ Thrash:SetCallback("OnSpawned", function()
 			TweenService:Create(neon, TweenInfo.new(1), {Color = targetColor}):Play()
 		end)
 	end
-end)
+end
+
+turnLightsRed()
+task.wait(1.5)
+
+-- 3. Create and run Thrash
+local Thrash = Spawner:Create({
+	Entity = {
+		Name = "Thrash",
+		Asset = "https://github.com/FireGiraffe/Violent-Mode-Doors/raw/main/Thrash.rbxm",
+		HeightOffset = 0
+	},
+	Movement = {
+		Speed = 280,
+		Delay = 0.6,
+		Reversed = false
+	},
+	Damage = {
+		Enabled = true,
+		IgnoreHiding = false,
+		Range = 65,
+		Amount = 125
+	},
+	Rebounding = {
+		Enabled = true,
+		Type = "Ambush",
+		Min = 3,
+		Max = 6,
+		Delay = 1.4
+	},
+	Lights = {
+		Flicker = {
+			Enabled = false,
+			Duration = 0
+		},
+		Shatter = false,
+		Repair = false
+	},
+	Earthquake = {
+		Enabled = false
+	},
+	CameraShake = {
+		Enabled = true,
+		Values = {3.2, 28, 0.08, 1.4},
+		Range = 120
+	},
+	Crucifixion = {
+		Type = "Curious",
+		Enabled = true,
+		Range = 66,
+		Resist = false,
+		Break = true
+	},
+	Death = {
+		Type = "Curious",
+		Hints = {
+			"Oh, hello...",
+			"You died to Thrash.",
+			"It moves around violently, coming back a few times.",
+			"His indication is the lights flickering a little more than usual.",
+			"Hope you don't mind trying again, it would be helpful."
+		},
+		Cause = "Thrash"
+	}
+})
 
 Thrash:Run(true)
